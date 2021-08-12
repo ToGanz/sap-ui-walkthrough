af300987b733035f0d2a7f7e035a7102cc826524
tganz <tganz5890@googlemail.com>
2021-08-12 06:58:09 +0200

    step 31: routing and navigation

--- webapp/Component.js
+++ webapp/Component.js
@@ -29,6 +29,9 @@ sap.ui.define(
             this._helloDialog = new HelloDialog(this.getRootControl());
           }.bind(this)
         );
+
+        // create the views based on the url/hash
+        this.getRouter().initialize();
       },
 
       exit: function () {
